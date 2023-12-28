87e7f41a585c02f9980c7fd93dddac11ea728ddb
radek <rajkonkret660@gmail.com>
2023-12-28 11:49:32 +0100

    update warunki

--- warunki.py
+++ warunki.py
@@ -38,3 +38,29 @@ rabat = 25 if suma_zam > 150 else 0
 print(f"Rabat wynosi {rabacik}")
 print(f"Rabat wynosi {rabat}")
 # 11:30
+
+lista_bledow = []
+alert_system = 'email'
+error = 'jakis inny'
+
+error_message = "Stało się coś strasznego"
+
+if alert_system == 'console':  # porównanie
+    print(error_message)
+elif alert_system == 'email':
+    lista_bledow.append('email')
+    if error == 'medium':
+        lista_bledow.append('medium')
+    elif error == 'critical':
+        lista_bledow.append('krytyczny')
+    else:
+        lista_bledow.append('inny')
+
+else:
+    print("System nieznany")
+
+print(lista_bledow)
+
+a = 14
+b = 3
+print(f"")
