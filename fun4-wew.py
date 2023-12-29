0f241e9ec94fc967ab97e3e8c01a9cf9a044743d
radek <rajkonkret660@gmail.com>
2023-12-29 10:54:17 +0100

    update fun4-wew

--- fun4-wew.py
+++ fun4-wew.py
@@ -16,3 +16,4 @@ print(xFun)  # <function fun1.<locals>.fun2 at 0x0000019532808CC0>
 print(type(xFun))  # <class 'function'>
 xFun()  # uruchomienie funkcji xFun czyli de facto fun2 bo to sie tam znajduje
 # To jest fun2
+# 11:25
