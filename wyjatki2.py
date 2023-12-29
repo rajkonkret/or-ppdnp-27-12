c92c651b2ef5ba400c3a4be7c74d8eeb8f84c124
radek <rajkonkret660@gmail.com>
2023-12-29 09:24:34 +0100

    add except in wyjatki2

--- wyjatki2.py
+++ wyjatki2.py
@@ -18,17 +18,25 @@ while True:
 
     if odp == "5":
         break
+    try:
+        a = int(input("Podaj pierwszą liczbę"))
+        b = int(input("Podaj drugą liczbę"))
 
-    a = int(input("Podaj pierwszą liczbę"))
-    b = int(input("Podaj drugą liczbę"))
-
-    if odp == "1":
-        print(f"Wynik dodawania {a} + {b} = {a + b}")
-    elif odp == "2":
-        print(f"Wynik odejmowania {a} - {b} = {a - b}")
-    elif odp == "3":
-        print(f"Wynik mnożenia {a} * {b} = {a * b}")
-    elif odp == "2":
-        print(f"Wynik dzielenia {a} / {b} = {a / b}")
-    else:
-        print("Nie znam takiego działania")
+        if odp == "1":
+            print(f"Wynik dodawania {a} + {b} = {a + b}")
+        elif odp == "2":
+            print(f"Wynik odejmowania {a} - {b} = {a - b}")
+        elif odp == "3":
+            print(f"Wynik mnożenia {a} * {b} = {a * b}")
+        elif odp == "2":
+            print(f"Wynik dzielenia {a} / {b} = {a / b}")
+        else:
+            print("Nie znam takiego działania")
+    except ZeroDivisionError:
+        print("Nie dziel przez zero")
+    except ValueError:
+        print("Bład wartości")
+    except Exception as e:
+        print("Bład", e)
+    finally:  # wykonuje się zawsze
+        print("Zakońćzone obliczenia")
