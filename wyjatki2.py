19dee04eb99976f224d2a7479d04e42f12c67929
radek <rajkonkret660@gmail.com>
2023-12-29 09:12:58 +0100

    add wyjatki2

---
+++ wyjatki2.py
@@ -0,0 +1,9 @@
+# napisanie aplikacji kalkulator
+# uzyc petli while jako głównej pętli programu
+# ma miec opcje wyjscia z programu
+# menu
+# pobierac typ operacji
+# ewentualnie pobrac liczby
+# łądnie wypisać wynik
+
+while True:
